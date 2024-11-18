26b088d473bb3b81b02ff73c1f995e35f0012208
Mindol7 <cgumgek8@dankook.ac.kr>
2024-11-18 19:31:06 +0100

    Assignment#01 - Get Secret Key after Paralyzation rand()

--- Assignment#02-Analysis_Conti_Ransomware/src/RSA_public_encrypt.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int RSA_public_encrypt(){
-	printf("RSA Function is OVERRIDING!!!\n");
-	printf("So RSA Function is PARALYZATION!!!\n");
-}
-
--- Assignment#02-Analysis_Conti_Ransomware/tools/RSA_public_encrypt.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int RSA_public_encrypt(int flen, const unsigned char *from, unsigned char *to, RSA *rsa, int padding){
-	printf("RSA Function is OVERRIDING!!!\n");
-	printf("So RSA Function is PARALYZATION!!!\n");
-}
-
